ba1491db7dea46d12a8be99b06286e94f90cc828
Elbeto379 <67297348+Elbeto379@users.noreply.github.com>
2022-05-14 16:08:47 -0400

    Funciones que devuelven mensajes

--- src/Backend.java
+++ src/Backend.java
@@ -19,4 +19,19 @@ public class Backend {
         }
     }
 
+    //One method is declared to return a message in case user's number is major than the mysterious number
+    protected final String getMajorNumberMessage() {
+        return "Your number is major than the mysterious number";
+    }
+
+    //One method is declared to return a message in case user's number is less than the mysterious number
+    protected final String getLess_number_message() {
+        return "Your number is less than the mysterious number";
+    }
+
+    //One method is declared in case the user's number is equals than de mysterious number
+    protected final String getEquals_number_message(){
+        return "Congratulations, you guessed the number";
+    }
+
 }
