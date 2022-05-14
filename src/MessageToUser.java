7a0fe2c61ebc34c51b1d37fd235d4a1100e9ac21
Elbeto379 <67297348+Elbeto379@users.noreply.github.com>
2022-05-14 14:34:42 -0400

    Creación de métodos que devuelven mensajes de acuerdo al número ingresado

---
+++ src/MessageToUser.java
@@ -0,0 +1,13 @@
+public class MessageToUser {
+
+    //One method is declared to return a message in case user's number is major than the mysterious number
+    public final String getMajorNumberMessage() {
+        return "Your number is major than the mysterious number";
+    }
+
+    //One method is declared to return a message in case user's number is less than the mysterious number
+    public final String getLess_number_message() {
+        return "Your number is less than the mysterious number";
+    }
+}
+
