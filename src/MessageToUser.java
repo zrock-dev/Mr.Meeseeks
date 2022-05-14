3c4f2b4e4b20ad553b394b250e22a74e5925f9c6
Elbeto379 <67297348+Elbeto379@users.noreply.github.com>
2022-05-14 14:43:49 -0400

    Messages to user

--- src/MessageToUser.java
+++ src/MessageToUser.java
@@ -9,5 +9,10 @@ public class MessageToUser {
     public final String getLess_number_message() {
         return "Your number is less than the mysterious number";
     }
+
+    //One method is declared in case the user's number is equals than de mysterious number
+    public final String getEquals_number_message(){
+        return "Congratulations, you guessed the number";
+    }
 }
 
