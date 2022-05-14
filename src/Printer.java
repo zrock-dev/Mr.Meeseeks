b664bea58a4d401eb6bd2a702333589aab5d0b9c
Elbeto379 <67297348+Elbeto379@users.noreply.github.com>
2022-05-14 17:14:59 -0400

    Mensajes de numeros

--- src/Printer.java
+++ src/Printer.java
@@ -20,17 +20,17 @@ public class Printer {
     }
 
     //One method is declared to return a message in case user's number is major than the mysterious number
-    public final String getMajorNumberMessage() {
+    protected final String getMajorNumberMessage() {
         return "Your number is more than the mysterious number";
     }
 
     //One method is declared to return a message in case user's number is less than the mysterious number
-    public final String getLess_number_message() {
+    protected final String getLess_number_message() {
         return "Your number is less than the mysterious number";
     }
 
     //One method is declared in case the user's number is equals than de mysterious number
-    public final String getEquals_number_message(){
+    protected final String getEquals_number_message(){
         return "Congratulations, you guessed the number";
     }
 
