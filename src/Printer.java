ae3c13f22e4ae78b683c284e7ec63b113e46864c
Elbeto379 <67297348+Elbeto379@users.noreply.github.com>
2022-05-14 16:50:31 -0400

    Mensajes de numeros

--- src/Printer.java
+++ src/Printer.java
@@ -18,4 +18,20 @@ public class Printer {
         System.out.println("*".repeat(CHARACTERS_HEIGHT));
         System.out.print("Enter a Number: "); // The input will be: Enter a number: 8
     }
+
+    //One method is declared to return a message in case user's number is major than the mysterious number
+    public final String getMajorNumberMessage() {
+        return "Your number is major than the mysterious number";
+    }
+
+    //One method is declared to return a message in case user's number is less than the mysterious number
+    public final String getLess_number_message() {
+        return "Your number is less than the mysterious number";
+    }
+
+    //One method is declared in case the user's number is equals than de mysterious number
+    public final String getEquals_number_message(){
+        return "Congratulations, you guessed the number";
+    }
+
 }
