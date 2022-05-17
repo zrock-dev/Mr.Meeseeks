9400aa49f941bf4fbee6b484070ca0ee92ef95c5
JhaelKatherine <101666151+JhaelKatherine@users.noreply.github.com>
2022-05-17 15:32:35 -0400

    Update Backend.java

--- src/Backend.java
+++ src/Backend.java
@@ -6,12 +6,12 @@ public class Backend {
     int LIMIT_NUMBER;
     int PLAYS_LIMIT;
 
-    Backend(Printer printer){  // Moved here to be able to use input number
+    Backend(Printer printer){
         this.printer = printer;
         LIMIT_NUMBER = 100;
         PLAYS_LIMIT = 3;
     }
-
+    //Receive the number entered by the user
     private int inputNumber(){
         Scanner scanner = new Scanner(System.in);
         int number;
@@ -24,12 +24,14 @@ public class Backend {
             }
         }
     }
-
+    //Generate a random number
     private int generateRandomNumber(){
         return (int) ((Math.random() * LIMIT_NUMBER) + 1);
     }
 
-
+    /*Checks if the number entered by the user is less than, greater than or equal
+     to the randomly generated number and returns a boolean value
+     */
     protected boolean compareNumbers(int userNumber, int hidden_number){
         // The boolean value is meant to control the guess loop.
 
@@ -43,20 +45,19 @@ public class Backend {
         }else{
             printer.showMoreThan();
         }
-        return true; // The forced exit
+        return true;
     }
-
+    // This method controls how many times the user will play the game.
     protected void runGame(){
         int plays = 0;
-        while(plays != PLAYS_LIMIT){ // Plays loop: is the # of times the user will play the game
+        while(plays != PLAYS_LIMIT){
             printer.showTitle(LIMIT_NUMBER);
             int hidden_number = generateRandomNumber();
 
             int userNumber = inputNumber();
             int tries = 1;
 
-            while(compareNumbers(userNumber, hidden_number)){ // guess loop
-                // Keeps going until the user guess the right number.
+            while(compareNumbers(userNumber, hidden_number)){
                 userNumber = inputNumber();
                 tries ++;
             }
