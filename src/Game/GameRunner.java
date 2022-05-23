b570a7dbd42d2978d79c756f1ec722892da14c5f
katherine <101666151+JhaelKatherine@users.noreply.github.com>
2022-05-23 08:04:45 -0400

    comments runGame

--- src/Game/GameRunner.java
+++ src/Game/GameRunner.java
@@ -13,6 +13,16 @@ public class GameRunner {
     }
 
 
+    /**
+     *This method is responsible for combining different methods to run the game,
+     * making use of control structures.
+     *<p>
+     * the method uses a for loop to control the number of times the user has to play
+     * the game, within the for the variables are initialized by calling the methods
+     * of other classes, it also has a while structure where if the condition is met,
+     * the user must enter another number again , and it also prints on the screen,
+     * the attempts in which the user had to find the random number.
+     */
     public void runGame(){
         int hiddenNumber;
         int userInput;
