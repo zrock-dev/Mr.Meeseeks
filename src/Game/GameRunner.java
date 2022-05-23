ec45831cc8a3db81eb39ac2b2326eb9ec7f3ed64
katherine <101666151+JhaelKatherine@users.noreply.github.com>
2022-05-23 08:12:56 -0400

    comments runGame

--- src/Game/GameRunner.java
+++ src/Game/GameRunner.java
@@ -17,7 +17,7 @@ public class GameRunner {
      *This method is responsible for combining different methods to run the game,
      * making use of control structures.
      *<p>
-     * the method uses a for loop to control the number of times the user has to play
+     * The method uses a for loop to control the number of times the user has to play
      * the game, within the for the variables are initialized by calling the methods
      * of other classes, it also has a while structure where if the condition is met,
      * the user must enter another number again , and it also prints on the screen,
